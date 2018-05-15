9d9bbe68b3212b30b732f3354acde266bb679dae
Mickaël Boucher <mickael.boucher7@gmail.com>
2018-05-15 10:14:23 +0200

    Projet 7, base

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+var map;
+
+// API google map
+function initMap() {
+			        var Lyon = {lat: 45.750000, lng: 4.850000};
+			        var map = new google.maps.Map(document.getElementById('map'), {
+			          zoom: 15,
+			          center: Lyon
+			        });
+			        var marker = new google.maps.Marker({
+			          position: Lyon,
+			          map: map
+			        });
+		restoJson();	        
+}
+
+function restoJson(){
+// pour récupérer les données du fichier json
+        $.getJSON('restaurants.json', function(restaurants){
+
+	          // pour faire une boucle sur chaque entrée du fichier json
+	        $.each(restaurants, function(index, restaurant){
+					
+
+					var marker = new google.maps.Marker({
+							          position: {lat: restaurant.lat, lng: restaurant.long},
+							          map: map
+					});
+	            
+
+	        });
+
+        })
+}
