3a148bcfeccea42b1377735a279e1352d74c4905
Mickaël Boucher <mickael.boucher7@gmail.com>
2018-05-16 14:36:54 +0200

    indentation + création main.js et map.js

--- js/map.js
+++ js/map.js
@@ -18,15 +18,15 @@ function restoJson(){
 
 // pour récupérer les données du fichier json
         $.getJSON('restaurants.json', function(restaurants){
-console.log("ok");
+
 	          // pour faire une boucle sur chaque entrée du fichier json
 	        $.each(restaurants, function(index, restaurant){
 					
 
-					var marker = new google.maps.Marker({
-							          position: {lat: restaurant.lat, lng: restaurant.long},
-							          map: map
-					});
+				var marker = new google.maps.Marker({
+		          position: {lat: restaurant.lat, lng: restaurant.long},
+		          map: map
+				});
 	            
 
 	        });
